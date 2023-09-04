0485a92d7f8d3d52c105450db14498a2baedb6d5
Bisquitman <lynx8309@gmail.com>
2023-09-04 21:45:34 +0300

    refactor 404 page

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -7,6 +7,7 @@ export const Layout = () => {
     <>
       <HelmetProvider>
         <Helmet>
+          <title>ToDo List App</title>
           <link rel="preconnect" href="https://fonts.googleapis.com"/>
           <link rel="preconnect" href="https://fonts.gstatic.com"/>
           <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;700&display=swap"
